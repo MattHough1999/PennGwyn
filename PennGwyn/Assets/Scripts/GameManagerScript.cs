dd1cef7577d3b4a78785fb8e3f87ba620eddd193
Jack Eatock <46260379+Jack-Eatock@users.noreply.github.com>
2021-12-11 11:52:36 +0000

    Basic menu

---
+++ PennGwyn/Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour
+{
+
+    public static GameManagerScript GameManager;
+
+
+    private void Awake()
+    {
+        if (GameManager != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            GameManager = this;
+            DontDestroyOnLoad(this);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}

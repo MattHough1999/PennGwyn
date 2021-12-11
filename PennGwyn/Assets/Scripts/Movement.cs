2fc9822d825c120dcb1021797390eeb014f53d0c
MattHough1999 <43546852+MattHough1999@users.noreply.github.com>
2021-12-11 16:04:22 +0000

    Penguin Slide With Sprite

--- PennGwyn/Assets/Scripts/Movement.cs
+++ PennGwyn/Assets/Scripts/Movement.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.IO;
+using UnityEngine.Sprites;
 
 
 
@@ -10,8 +11,16 @@ using UnityEngine.IO;
 
 public class Movement : MonoBehaviour
 {
+    private SpriteRenderer rend;
+
+    public BoxCollider2D boxStanding;
+    public BoxCollider2D boxSliding;
+    public Sprite SpriteStand;
+    public Sprite SpriteSlide;
     Rigidbody2D rb = new Rigidbody2D();
     bool moving = false;
+    bool grounded = true;
+    bool sliding = false;
     public Vector2 MoveDir = Vector2.zero;
     public Vector2 vel = Vector2.zero;
     float maxSpeed = 1;
@@ -21,27 +30,52 @@ public class Movement : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         MoveDir = new Vector2(0, 0);
+        rend = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
        
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
         {
             rb.velocity = new Vector2(0.0f, 5.0f);
+            grounded = false;
             
         }
         if (Input.GetKey(KeyCode.A))
         {
-            rb.velocity = new Vector2(-2.0f, 0.0f);
+            rend.flipX = false;
+            rb.velocity = new Vector2(-2.0f, rb.velocity.y);
 
         }
         if (Input.GetKey(KeyCode.D))
         {
-            rb.velocity = new Vector2(2.0f, 0.0f);
+            rend.flipX = true;
+            rb.velocity = new Vector2(2.0f, rb.velocity.y);
 
         }
+        if (Input.GetKeyDown(KeyCode.S)) 
+        {
+            sliding = true;
+            boxSliding.enabled = true;
+            boxStanding.enabled = false;
+            rb.velocity = new Vector2(rb.velocity.x * 4, rb.velocity.y);
+            rend.sprite = SpriteSlide;
+
+
+        }
+        if (Input.GetKeyUp(KeyCode.S)) { sliding = false; }
+        if (sliding == false) 
+        {
+            rend.sprite = SpriteStand;
+            boxStanding.enabled = true;
+        }
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Floor") grounded = true;
         
     }
 }

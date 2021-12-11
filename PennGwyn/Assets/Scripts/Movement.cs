bb0569aa9112a0a3ff2ff33589325347d1ca32cd
MattHough1999 <43546852+MattHough1999@users.noreply.github.com>
2021-12-11 14:00:18 +0000

    PenGuin falls
    the penguin now falls

---
+++ PennGwyn/Assets/Scripts/Movement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.IO;
+
+
+
+[RequireComponent(typeof(Rigidbody))]
+
+
+public class Movement : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+            
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
